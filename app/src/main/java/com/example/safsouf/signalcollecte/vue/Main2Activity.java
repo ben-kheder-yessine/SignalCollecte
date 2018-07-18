f804952beea9335c2bdeda86e8bd16134f89b127
ROukaya\roukaya <bensalahroukaya@gmail.com>
2018-07-18 19:47:20 +0200

    change interface

--- app/src/main/java/com/example/safsouf/signalcollecte/vue/Main2Activity.java
+++ app/src/main/java/com/example/safsouf/signalcollecte/vue/Main2Activity.java
@@ -1,50 +1,64 @@
 package com.example.safsouf.signalcollecte.vue;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
+import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.text.Selection;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.safsouf.signalcollecte.R;
 import com.example.safsouf.signalcollecte.modele.ConnectionDetector;
 import com.example.safsouf.signalcollecte.modele.Debit;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
 
     TelephonyManager mTelephonyManager;
     MyPhoneStateListener mPhoneStatelistener;
     String mSignalStrength = "";
-    EditText txt_etat;
+    TextView txt_etat;
     EditText txt_Name_Device;
-    int speedMbps;
     ConnectionDetector cd;
-    Debit db;
-    EditText  rsrp;
+    EditText rsrp;
     EditText rsrq;
     EditText rssi;
     EditText cqi;
     EditText SS;
-    EditText  dn;
-    EditText  dm;
-    EditText  dv;
+    EditText dn;
+    EditText dm;
+    EditText dv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        txt_etat=(EditText)findViewById( R.id.txt_etat );
+        txt_etat = (TextView) findViewById(R.id.etat);
         rsrp = (EditText) findViewById(R.id.rsrp);
         rsrq = (EditText) findViewById(R.id.rsrq);
-        rssi= (EditText) findViewById(R.id.rssi);
+        rssi = (EditText) findViewById(R.id.rssi);
         cqi = (EditText) findViewById(R.id.cqi);
         SS = (EditText) findViewById(R.id.ss);
-       dn = (EditText) findViewById(R.id.dn);
-       dm= (EditText) findViewById(R.id.dm);
+        dn = (EditText) findViewById(R.id.dn);
+        dm = (EditText) findViewById(R.id.dm);
         dv = (EditText) findViewById(R.id.dv);
+
+        PhoneStateListener myPhoneStateListener;
+        TelephonyManager tm;
 
         /*cd= new ConnectionDetector( this );
            if(cd.isConnected()){
@@ -58,14 +72,15 @@ public class Main2Activity extends AppCompatActivity {
         //Device name
         String manufacturer = Build.MANUFACTURER;
         dn.setText(String.valueOf(manufacturer));
-        Log.d("name----->", String.valueOf( manufacturer));
+        Log.d("name----->", String.valueOf(manufacturer));
         //Device model
         String model = Build.MODEL;
         dm.setText(String.valueOf(model));
-        Log.d("model----->", String.valueOf( model));
+        Log.d("model----->", String.valueOf(model));
 
         // Device version
         String version = Build.VERSION.RELEASE;
+        dv.setText("Android");
         dv.setText(String.valueOf(version));
         Log.d("version----->", String.valueOf( version));
         }
@@ -84,37 +99,39 @@ public class Main2Activity extends AppCompatActivity {
             int rssnrLTE = 0;
             int cqiLTE = 0;
 
-            if ( mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_LTE){
+            if (mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_LTE) {
                 txt_etat.setText("Conected to LTE");
-                } else {
+            } else {
                 txt_etat.setText("Not conected to LTE");
-                }
+            }
 
             // For Lte SignalStrength: dbm = ASU - 140.
             //Log.d("signalStrength----->", String.valueOf(mSignalStrength));
-           //SS.setText(String.valueOf(mSignalStrength));
+            //SS.setText(String.valueOf(mSignalStrength));
 
             //LteSignalStrength PART 8
-            signalStrengthDbm = Integer.parseInt(parts[8])-140;
+            signalStrengthDbm = Integer.parseInt(parts[8]) - 140;
             SS.setText(String.valueOf(signalStrengthDbm));
 
             //RSRP PART 9
-            rsrpLTE = Integer.parseInt(parts[9])-140;
+            rsrpLTE = Integer.parseInt(parts[9]) - 140;
             rsrp.setText(String.valueOf(rsrpLTE));
 
             //RSRQPART 10
-            rsrqLTE = Integer.parseInt(parts[10])-140;
+            rsrqLTE = Integer.parseInt(parts[10]) - 140;
             rsrq.setText(String.valueOf(rsrqLTE));
 
             //RSSNR PART 11
-            rssnrLTE = Integer.parseInt(parts[11])-140;
+            rssnrLTE = Integer.parseInt(parts[11]) - 140;
             rssi.setText(String.valueOf(rssnrLTE));
 
             //CQI  PART 12
-            cqiLTE = Integer.parseInt(parts[12])-140;
+            cqiLTE = Integer.parseInt(parts[12]) - 140;
             cqi.setText(String.valueOf(cqiLTE));
-
-            }
-
+        }
     }}
+
+
+
+
 
--- app/src/main/java/com/example/safsouf/signalcollecte/vue/MainActivity.java
+++ app/src/main/java/com/example/safsouf/signalcollecte/vue/MainActivity.java
@@ -23,24 +23,16 @@ public class MainActivity extends AppCompatActivity {
                 }
         });    }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         init();
-
-
         Spinner mySpinner= (Spinner)findViewById(R.id.spinner1);
         ArrayAdapter<String>myAdapter=new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
-
-
-
     }
 }
